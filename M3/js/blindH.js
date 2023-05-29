981ba39386c33af655bcc2bf47af1d529a9a8cf9
mohamedelfarouk <mohameda.elfarouk@gmail.com>
2023-05-29 07:52:35 +0300

    trying bonus

---
+++ M3/js/blindH.js
@@ -0,0 +1,26 @@
+const texts = document.querySelector(".texts");
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement("p");
+
+recognition.addEventListener("result", (e) => {
+  texts.appendChild(p);
+  const text = Array.from(e.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join("");
+
+  if (text === "Google") {
+    window.location.href = "login.html";
+  } else {
+  }
+
+  p.innerText = text;
+});
+
+recognition.start();
---
+++ M3/js/blindLogin.js
@@ -0,0 +1,23 @@
+const texts = document.querySelector(".texts");
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+recognition.addEventListener("result", (e) => {
+  const text = Array.from(e.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join("");
+
+  if (text === "Google") {
+    window.location.href = "blindH.html";
+  } else {
+  }
+
+  p.innerText = text;
+});
+
+recognition.start();
